efb6072639e1fe87319ead91cd31efd9d8152be9
nhuanh9 <chopperlouis@gmail.com>
2022-11-13 15:42:46 +0700

    done build routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import logo from './logo.svg';
+import {Routes, Route, Link} from "react-router-dom";
 import './App.css';
+import Admin from "./pages/admin/Admin";
+import React from "react";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import User from "./pages/user/User";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+
+            <Routes>
+                <Route path="/" element={<Login/>}/>
+                <Route path="/register" element={<Register/>}/>
+                <Route path="/user" element={<User/>}/>
+                <Route path="/admin" element={<Admin/>}/>
+                <Route path="*" element={<Login/>}/>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
