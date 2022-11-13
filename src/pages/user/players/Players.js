ca789693f1e883644fad1169c31517488c6a4385
nhuanh9 <chopperlouis@gmail.com>
2022-11-13 18:08:50 +0700

    done show all players

--- src/pages/user/players/Players.js
+++ src/pages/user/players/Players.js
@@ -1,5 +1,42 @@
+import React, {useEffect} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {getPlayers} from "../../../services/playerService";
+
 export default function Players() {
+    const dispatch = useDispatch()
+    const {players} = useSelector((state) => {
+            return state.players
+        }
+    )
+    useEffect(() => {
+        dispatch(getPlayers())
+    }, []);
     return (
-        <h1>List Players</h1>
+        <div className={'row'}>
+            <div className="col-2">Side Bar</div>
+            <div className="col-10 mt-2">
+                <h1>List Players</h1>
+                <table className="table table-striped">
+                    <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Champ</th>
+                        <th scope="col">KDA</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {players.map((item, index) => (
+                        <tr>
+                            <th scope="row">{index+1}</th>
+                            <td>{item.name}</td>
+                            <td>{item.champ}</td>
+                            <td>{item.kda}</td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
+            </div>
+        </div>
     )
 }
