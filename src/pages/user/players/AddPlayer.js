bacb4c8afaa34b781a937fce89edcf8b3a52c5a2
nhuanh9 <chopperlouis@gmail.com>
2022-11-14 00:03:34 +0700

    done validate name

--- src/pages/user/players/AddPlayer.js
+++ src/pages/user/players/AddPlayer.js
@@ -1,39 +1,47 @@
-import {Field, Form, Formik} from "formik";
+import {ErrorMessage, Field, Form, Formik} from "formik";
 import React from "react";
 import {useNavigate} from "react-router";
 import {useDispatch} from "react-redux";
 import {addPlayer} from "../../../services/playerService";
-
+import * as yup from 'yup';
 export default function AddPlayer() {
     const navigate = useNavigate();
     const dispatch = useDispatch()
     const handleSubmit = async (data) => {
-       await dispatch(addPlayer(data));
-       navigate('/user')
+        await dispatch(addPlayer(data));
+        navigate('/user')
     }
+    const signUpSchema = yup.object().shape({
+        name: yup.string()
+            .min(2, "Too Short!")
+            .max(50, "Too Long!")
+            .required("Firstname is required"),
+    })
     return (
         <div className={'row'}>
             <div className={'offset-2 col-6'}>
                 <h1>Add new players</h1>
-            <Formik
-                initialValues={
-                    {
-                        name: '',
-                        champ: '',
-                        kda: '',
-                        des: ''
+                <Formik
+                    initialValues={
+                        {
+                            name: '',
+                            champ: '',
+                            kda: '',
+                            des: ''
+                        }
                     }
-                }
-                onSubmit={(values) => handleSubmit(values)}
-            >
-                <Form className={'form-group d-flex justify-content-center flex-column'}>
-                    <Field name={'name'} className={'form-control mb-2'}></Field>
-                    <Field name={'champ'} className={'form-control mb-2'}></Field>
-                    <Field name={'kda'} className={'form-control mb-2'}></Field>
-                    <Field name={'des'} className={'form-control mb-2'}></Field>
-                    <button className={'btn btn-primary'}>Save</button>
-                </Form>
-            </Formik>
+                    onSubmit={(values) => handleSubmit(values)}
+                    validationSchema={signUpSchema}
+                >
+                    <Form className={'form-group d-flex justify-content-center flex-column'}>
+                        <Field name={'name'} className={'form-control mb-2'}></Field>
+                        <ErrorMessage name={'name'}/>
+                        <Field name={'champ'} className={'form-control mb-2'}></Field>
+                        <Field name={'kda'} className={'form-control mb-2'}></Field>
+                        <Field name={'des'} className={'form-control mb-2'}></Field>
+                        <button className={'btn btn-primary'}>Save</button>
+                    </Form>
+                </Formik>
             </div>
         </div>
     )
